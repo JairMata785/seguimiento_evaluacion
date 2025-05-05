64de75173e0b32f42cce867863b1e333d02f5fec
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-05-05 00:20:55 -0600

    seguimiento

--- seguimiento_evaluacion/evaluaciones/urls.py
+++ seguimiento_evaluacion/evaluaciones/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_views, docente_views, jefe_views
+from .views import docente_views, jefe_views, home_views
 
 urlpatterns = [
     path('', home_views.home, name='home'),
@@ -8,6 +8,8 @@ urlpatterns = [
     path('docente/autoevaluacion/', docente_views.autoevaluacion, name='autoevaluacion'),
     path('docente/reporte/<int:evaluacion_id>/', docente_views.descargar_reporte, name='descargar_reporte'),
     path('docente/mis-evaluaciones/', docente_views.ver_mis_evaluaciones, name='ver_mis_evaluaciones'),
+    path('lista-docentes/', home_views.lista_docentes, name='lista_docentes'),
+    path('detalle-docente/<int:docente_id>/', home_views.detalle_docente, name='detalle_docente'),
     
     # Rutas de jefe departamental
     path('jefe/evaluacion/<int:docente_id>/', jefe_views.evaluacion_departamental, name='evaluacion_departamental'),
